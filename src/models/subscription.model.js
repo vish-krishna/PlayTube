29f3183f77013f586c29ba6cf04363c567a083dd
Krishna <krishnakrvish@gmail.com>
2024-09-06 23:49:28 +0530

    add tweet, like, comment and playlist model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,14 +1,14 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema } from 'mongoose';
 
 const subscriptionSchema = new Schema(
     {
         subscriber: {
             type: Schema.Types.ObjectId, // who is subscribing
-            ref: "User",
+            ref: 'User',
         },
         channel: {
             type: Schema.Types.ObjectId, // who is subscribed
-            ref: "User",
+            ref: 'User',
         },
     },
     {
@@ -16,4 +16,4 @@ const subscriptionSchema = new Schema(
     }
 );
 
-export const Subscription = mongoose.model("Subscription", subscriptionSchema);
+export const Subscription = mongoose.model('Subscription', subscriptionSchema);
